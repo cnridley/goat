138429b2c9d11bb34faa5314323408fb259d914a
cnridley <cnridley1@gmail.com>
2021-05-24 02:45:30 +0000

    added models for features section and insights

--- devTest1/models.py
+++ devTest1/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Feature(models.Model):
+    featureCategory = models.CharField(max_length=254, null=True, blank=True)
+    feautureTitle = models.CharField(max_length=254, null=True, blank=True)
+    featureDesc = models.CharField(max_length=254, null=True, blank=True)
+    featureImage = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.featureCategory
+
+
+class Insights(models.Model):
+    insightsCategory = models.CharField(max_length=254, null=True, blank=True)
+    insightsTitle = models.CharField(max_length=254, null=True, blank=True)
+    insightsDesc = models.TextField(max_length=254, null=True, blank=True)
+    insightsImage = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.insightsCategory
--- devTest1/views.py
+++ devTest1/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Feature, Insights
 
 # Create your views here.
+
 
 def index(request):
 
     """A view to return index page"""
+    feature = Feature.objects.all()
+    insights = Insights.objects.all()
 
-    return render(request, 'index.html')
+    context = {
+        'feature': feature,
+        'insights': insights,
+    }
+
+    return render(request, 'index.html', context)
